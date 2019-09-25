81d283313844988ae8e6518e988f716ec83dfd34
Omar Rodriguez <omar.m.rodriguez1@outlook.com>
2019-09-25 13:57:42 -0700

    Added a SQL database.

---
+++ SQLQuery1.sql
@@ -0,0 +1,13 @@
+﻿CREATE DATABASE [Encryption]
+GO
+
+USE [Encryption]
+GO
+
+CREATE TABLE [Users] (
+    [UserID]   int NOT NULL IDENTITY,
+    [Username] nvarchar(max) NOT NULL,
+	[Password] nvarchar(max) NOT NULL,
+    PRIMARY KEY ([UserID])
+);
+GO
